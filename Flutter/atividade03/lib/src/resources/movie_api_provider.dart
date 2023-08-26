4c294bf829189b76c1e76af30f4ea06fdb33b772
Abraão Albuquerque <abraao.hs2@gmail.com>
2023-08-26 18:28:53 -0300

    :lipstick: costumizando cards e usando dados que vem da API

---
+++ Flutter/atividade03/lib/src/resources/movie_api_provider.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../models/item_model.dart';
+
+class MovieApiProvider {
+  final _apiKey = '7204bf3a518825b385b085dbc0700817';
+
+  Future<ItemModel> fetchMovieList() async {
+    final url =
+        Uri.parse("http://api.themoviedb.org/3/movie/popular?api_key=$_apiKey");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      // If the call to the server was successful, parse the JSON
+      return ItemModel.fromJson(json.decode(response.body));
+    } else {
+      // If that call was not successful, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
+}
---
+++ Flutter/atividade03/lib/src/ui/movie_list.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/movies_bloc.dart';
+
+class MovieList extends StatelessWidget {
+  @override
+  Widget build(context) {
+    moviesBloc.fetchAllMovies();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Popular Movies'),
+      ),
+      body: StreamBuilder(
+        stream: moviesBloc.allMovies,
+        builder: (context, AsyncSnapshot<ItemModel> snapshot) {
+          if (snapshot.hasData) {
+            return buildList(context, snapshot);
+          } else if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+
+  Widget buildList(context, AsyncSnapshot<ItemModel> snapshot) {
+    return SingleChildScrollView(
+      child: Container(
+        width: double.infinity,
+        child: Wrap(
+            alignment: WrapAlignment.spaceAround,
+            children: (List.generate(
+                snapshot.data?.results.length ?? 0,
+                (index) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: movieCard(snapshot.data?.results[index] as Result),
+                    )))),
+      ),
+    );
+  }
+}
+
+Color getNoteColor(double note) {
+  if (note < 0) {
+    note = 0;
+  } else if (note > 10) {
+    note = 10;
+  }
+
+  final double ratio = note / 10.0;
+
+  final int red = (225 * (1 - ratio)).toInt();
+  final int green = (225 * ratio).toInt();
+
+  return Color.fromARGB(255, red, green, 100);
+}
+
+Widget movieCard(Result data) {
+  return ClipRRect(
+    borderRadius: BorderRadius.circular(8.0),
+    child: Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.0),
+          border:
+              Border.all(color: const Color.fromARGB(50, 0, 0, 0), width: 2)),
+      constraints: const BoxConstraints(
+        maxWidth: 300,
+      ),
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.network(
+              'https://image.tmdb.org/t/p/w185${data.poster_path}',
+              fit: BoxFit.fitWidth,
+              width: 600,
+            ),
+          ),
+          Positioned(
+            width: 36,
+            height: 36,
+            top: 8,
+            left: 8,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                border: Border.all(
+                    color: const Color.fromARGB(255, 200, 200, 200), width: 1),
+                color: getNoteColor(data.vote_average),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Center(
+                  child: Text('${data.vote_average}',
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold)),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
